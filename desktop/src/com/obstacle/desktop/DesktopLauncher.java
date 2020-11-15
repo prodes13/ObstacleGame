9f067ac4a96c1949438146193d778f63223bc715
prodes13 <ciobotaru.florin@gmail.com>
2020-11-15 10:33:53 +0200

    first commmit

---
+++ desktop/src/com/obstacle/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.obstacle.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.obstacle.ObstacleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ObstacleGame(), config);
+	}
+}
